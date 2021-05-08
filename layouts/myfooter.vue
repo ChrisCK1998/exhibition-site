29bf5bf964b1de609cc78b3b05074c093065ca41
ChrisCK1998 <ckai98@qq.com>
2021-05-09 01:42:27 +0800

    Initial commit

---
+++ layouts/myfooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer-container">
+    <div class="wrapper">
+      <div><span class="record">嘉兴学院-软件工程-陈开</span><span
+        class="phone">15971564139</span></div>
+      <div class="right"><span
+        class="v-link clickable"> 联系我们 </span><span
+        class="v-link clickable"> 合作伙伴 </span><span
+        class="v-link clickable"> 用户协议 </span><span
+        class="v-link clickable"> 隐私协议 </span></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
